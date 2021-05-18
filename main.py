1f0572c86715d7898db4c07992f2369ef5df2cfc
sandeepvura <>
2021-05-18 19:27:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+days = 365
+weeks = 52
+months = 12
+age_conv = int(age)
+days_left = print(f"You have {days * age_conv}, {weeks * age_conv} , and {months * age_conv} months left.")
+
+#You can use any arthematic operator in my case i used multiplication
+
+
+
+
+
